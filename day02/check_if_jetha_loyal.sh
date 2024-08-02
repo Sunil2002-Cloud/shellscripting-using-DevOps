ff269733089837180dfa8d7a7fdcbea1c20dc5b5
Ubuntu <ubuntu@ip-172-31-32-224.ap-south-1.compute.internal>
2024-08-02 20:25:27 +0000

    Added day02

---
+++ day02/check_if_jetha_loyal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<< disclaimer
+This is just for infotainment purpose
+disclaimer
+
+read -p "jetha ne mud ke kise dekha: " bandi
+read -p "jetha ka pyar %" pyaar
+
+if [[ $bandi == "daya bhabhi" ]];
+then
+
+         echo "jetha is loyal"
+elif [[ $pyaar -ge 100 ]];
+then         echo "jetha is  loyal"
+else
+             echo "jetha is not loyal"
+fi
---
+++ day02/jetha_lal_ki_duniya.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This is Jetha Lal Ki Duniya
+
+<< comment
+Anything
+written
+here will not be execute
+comment
+
+
+name="babitaji"
+
+echo "Name is $name"
+
+echo "Name is $name, and date is $(date)"
+
+echo "enter the name:"
+
+read username
+echo "You  entered $username"
+
+echo "The characters in $0 are: $1 $2"
